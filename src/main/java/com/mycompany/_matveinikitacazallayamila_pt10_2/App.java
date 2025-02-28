bb4677037a8f899c1e104f5386b818b1f056b6cd
nikit <nikit@MateBook_Nikita>
2025-02-28 08:10:58 +0100

    Initial version

---
+++ src/main/java/com/mycompany/_matveinikitacazallayamila_pt10_2/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._matveinikitacazallayamila_pt10_2;
+
+/**
+ *
+ * @author nikit
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
